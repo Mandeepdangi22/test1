ad4265f6e3b36974365951fc832613ca21a5d33e
Mandeepdangi22 <mandeepdangi22@gmail.com>
2024-10-26 17:47:52 +0530

    first commit

---
+++ src/assets/Assests.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AuthDeltaVelocityLogo  from "./auth/AuthDeltaVelocityLogo.svg";  
+import AuthCarouselImage1  from "./auth/AuthCarouselImage1.svg";  
+import AuthCarouselImage2  from "./auth/AuthCarouselImage2.svg";  
+import AuthCarouselImage3  from "./auth/AuthCarouselImage3.svg"; 
+import  AuthIndianFlag from "./auth/AuthIndianFlag.svg";  
+import AuthOrangeTick  from "./auth/AuthOrangeTick.svg";  
+import AuthOtpIcon  from "./auth/AuthOtpIcon.svg";  
+import AuthPasswordVisiblity  from "./auth/AuthPasswordVisiblity.svg";  
+
+
+export default{
+  AuthCarouselImage1,
+  AuthCarouselImage2,
+  AuthCarouselImage3,
+  AuthDeltaVelocityLogo,
+  AuthIndianFlag,
+  AuthOrangeTick,
+  AuthOtpIcon,
+  AuthPasswordVisiblity
+}
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,28 @@
+// import { sidebarSlice } from "../views/afterAuth/Dashboard/sidebar/redux/reducer";
+// import { authSlice } from "../views/beforeAuth/login/Redux/reducer";
+// import { signupSlice } from "../views/beforeAuth/signup/Redux/reducer";
+// import { brandSlice } from "../views/afterAuth/Dashboard/Setup/components/KYC/Redux/reducer";
+// import { wareHouseSlice } from "../views/afterAuth/Dashboard/Setup/components/Warehouse/Redux/reducer";
+// import { userStatusSlice } from "../views/afterAuth/Dashboard/Redux/reducer";
+// import { channelSlice } from "../views/afterAuth/Dashboard/Setup/components/Channel/Redux/reducer";
+// import { skuSlice } from "../views/afterAuth/Dashboard/Sku/Redux/reducer";
+// import { pricingSlice } from "../views/afterAuth/Dashboard/Setup/components/Pricing/Redux/reducer";
+// import { ordersSlice } from "../views/afterAuth/Dashboard/Orders/Redux/reducer";
+// import { reconSlice } from "../views/afterAuth/Dashboard/Reconcilitaion/Redux/reducer";
+// import { integrationSlice } from "../views/afterAuth/Dashboard/Setting/pages/integration/Redux/reducer";
+// import { labelSlice } from "../views/afterAuth/Dashboard/Setting/pages/label&Tracking/Redux/reducer";
+export const rootReducer = {
+  // sidebar: sidebarSlice.reducer,
+  // auth: authSlice.reducer,
+  // signup: signupSlice.reducer,
+  // warehouse: wareHouseSlice.reducer,
+  // brand: brandSlice.reducer,
+  // userStatus: userStatusSlice.reducer,
+  // channel: channelSlice.reducer,
+  // sku: skuSlice.reducer,
+  // pricing: pricingSlice.reducer,
+  // orders: ordersSlice.reducer,
+  // recon: reconSlice.reducer,
+  // integration: integrationSlice.reducer,
+  // label: labelSlice.reducer,
+};
